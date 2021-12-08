a8e953916f2c6c8e9a39a6f51b0bba16d134cd72
kitaiskypanda <kitaiskypanda@gmail.com>
2021-12-08 18:51:31 +0300

    port 80

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ data = {
     "payload": 'комиссия надбавка зависит от того, где и сколько паев открытого фонда "Рога и копыта" вы покупаете'
 }
 
-ans = requests.post('http://127.0.0.1:8000/app', data=json.dumps(data)).content
+ans = requests.post('http://127.0.0.1:80/app', data=json.dumps(data)).content
 
 print(json.loads(ans.decode('utf-8'))['payload'])
